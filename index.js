6b905f76a6beda4c6f28e6fb700995b7204d0e22
Simme63 <simon.wandel@hotmail.com>
2025-01-12 23:35:15 +0100

    try 2

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/notes", notesRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 app.get("/", (req, res) => {
   res.send("API is running")
 })
