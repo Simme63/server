69f7f07689246fb6dee94e38248ff1e1c66a21b1
Simme63 <simon.wandel@hotmail.com>
2025-01-13 11:39:18 +0100

    blub lbub

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(morgan("dev")); // Logs HTTP requests
 // Or, you can specify which origins are allowed, like your React frontend:
 app.use(cors({
-  origin: "https://your-react-frontend-url.com",  // The URL of your deployed React app
+  origin: "https://client-hknlo60xz-simme63s-projects.vercel.app/",  // The URL of your deployed React app
   methods: "GET,POST,PUT,DELETE",               // Allow the necessary methods
   allowedHeaders: "Content-Type,Authorization",  // Allow specific headers
 }));
